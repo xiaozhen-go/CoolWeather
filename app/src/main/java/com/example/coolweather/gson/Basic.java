37c1ebcc2b35720483c98a1fe3099b3334dcc9fa
xiaozhen-go <1213417148@qq.com>
2019-03-10 18:09:07 +0800

    酷欧天气

--- app/src/main/java/com/example/coolweather/gson/Basic.java
+++ app/src/main/java/com/example/coolweather/gson/Basic.java
@@ -2,7 +2,7 @@ package com.example.coolweather.gson;
 
 import com.google.gson.annotations.SerializedName;
 
-public class Basic {
+public class  Basic {
 
     @SerializedName("city")
     public String cityName;
